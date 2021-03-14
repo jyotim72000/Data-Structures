3f8d863390dbe7d61b21fa417312ba96b1affc08
jyotim72000 <71043626+jyotim72000@users.noreply.github.com>
2021-03-14 15:58:18 +0530

    Finding maximum value of the given Binary Tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -16,22 +16,46 @@ public class BinaryTree
 	
 	public void createBinaryTree()
 	{
-		TreeNode first = new TreeNode(9);
+		TreeNode first = new TreeNode(1);
 		TreeNode second = new TreeNode(2);
-		TreeNode third = new TreeNode(3);
+		TreeNode third = new TreeNode(8);
 		TreeNode fourth = new TreeNode(4);
+		TreeNode fifth = new TreeNode(5);
+		TreeNode sixth = new TreeNode(6);
+		TreeNode seventh = new TreeNode(7);
 		
 		root = first; // root----> first
 		first.left = second;
 		first.right = third; // second<--- first ---> third
-		
 		second.left = fourth;
+		second.right = fifth;
+		third.left = sixth;
+		third.right = seventh;
 	}
 	
+	public int findMax() 
+	{
+		return findMax(root);
+	}
+	
+	public int findMax(TreeNode root)
+	{
+		if(root ==  null)
+			return Integer.MIN_VALUE;
+		int result = root.data;
+		int left = findMax(root.left);
+		int right = findMax(root.right);
+		if(left > result)
+			result = left;
+		if(right > result)
+			result = right;
+		return result;
+	}
 	
 	public static void main(String[] args)
 	{
 		BinaryTree bt = new BinaryTree();
 		bt.createBinaryTree();
+		System.out.print("Maximum value of the given binary tree is: " + bt.findMax());
 	}
 }
